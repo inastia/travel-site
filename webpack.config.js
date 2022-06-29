078a659481af8d55f75b531e7c373739c59cf4fa
nastia <nastia.bigun@gmail.com>
2022-06-29 14:28:17 -0600

    CSS Architechture
    class naming according to BEM - block element modifier

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,26 @@
-const path = require('path');
+const path = require('path')
+
 const postCSSPlugins = [
-  require("postcss-simple-vars"),
-  require("postcss-nested"),
-  require("autoprefixer")
+  require('postcss-import'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
 ]
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: './app/assets/scripts/App.js',
   output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "app")
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
   },
-  mode: "development",
+  mode: 'development',
   watch: true,
   module: {
     rules: [
-      // object
       {
-        // only if the file ends in css
         test: /\.css$/i,
-        // use the loader
-        use: ['style-loader', 'css-loader?url=false', {
-          loader: "postcss-loader",  
-          options: { postcssOptions: {
-            plugins: postCSSPlugins }
-          }
-        }]
+        use: ['style-loader', 'css-loader?url=false', { loader: 'postcss-loader', options: { postcssOptions: { plugins: postCSSPlugins } } }]
       }
-    ]
-  }
+    ] // rules end
+  } // module ends 
 }
